cbdf08bc804cf92f636681cfe5aa8a34221e80ec
LawrenceZhou <hmskyijun@gmail.com>
2017-02-21 14:15:48 -0800

    1.1

--- components/user-list/user-listController.js
+++ components/user-list/user-listController.js
@@ -3,6 +3,7 @@
 cs142App.controller('UserListController', ['$scope',
     function ($scope) {
         $scope.main.title = 'Users';
+        $scope.nameList = window.cs142models.userListModel();
 
         console.log('window.cs142models.userListModel()', window.cs142models.userListModel());
     }]);
