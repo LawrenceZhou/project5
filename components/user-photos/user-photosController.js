765bae52b29752e2a3d01824abec29061847a5d8
LawrenceZhou <hmskyijun@gmail.com>
2017-02-22 17:17:33 -0800

    1.4

--- components/user-photos/user-photosController.js
+++ components/user-photos/user-photosController.js
@@ -12,6 +12,9 @@ cs142App.controller('UserPhotosController', ['$scope', '$routeParams',
     var lastName = window.cs142models.userModel(userId).last_name;
     $scope.main.toolBar = "Photos of " + firstName + " " + lastName;
 
+    //$scope.photos = {};
+    $scope.photos = window.cs142models.photoOfUserModel(userId));
+
     console.log('window.cs142models.photoOfUserModel($routeParams.userId)',
        window.cs142models.photoOfUserModel(userId));
 
