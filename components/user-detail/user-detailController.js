d124808e7712451bf2f91b605ea61ca66564f153
LawrenceZhou <hmskyijun@gmail.com>
2017-02-22 16:44:13 -0800

    1.3

--- components/user-detail/user-detailController.js
+++ components/user-detail/user-detailController.js
@@ -6,11 +6,17 @@ cs142App.controller('UserDetailController', ['$scope', '$routeParams',
      * Since the route is specified as '/users/:userId' in $routeProvider config the
      * $routeParams  should have the userId property set with the path from the URL.
      */
+
     var userId = $routeParams.userId;
     console.log('UserDetail of ', userId);
     var firstName = window.cs142models.userModel(userId).first_name;
     var lastName = window.cs142models.userModel(userId).last_name;
     $scope.main.toolBar = firstName + " " + lastName;
+    $scope.user = {};
+    $scope.user.name = firstName + " " + lastName;
+    $scope.user.location = window.cs142models.userModel(userId).location;
+    $scope.user.occupation = window.cs142models.userModel(userId).occupation;
+    $scope.user.discription = window.cs142models.userModel(userId).discription;
     console.log('window.cs142models.userModel($routeParams.userId)',
         window.cs142models.userModel(userId));
 
